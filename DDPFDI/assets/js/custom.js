f0ab12d35a69f52d41c44b212f7e2db8c4b5781a
Shiv <kumarshiva.singh30@gmail.com>
2019-04-12 13:10:33 +0530

    Siderbar Active menu functionality

--- DDPFDI/assets/js/custom.js
+++ DDPFDI/assets/js/custom.js
@@ -17,6 +17,18 @@
 
     $(".sidebar-holder .parent-nav").click(function(){
         $(this).find('.parent-nav-child').slideToggle();
+        $(this).siblings().find('.parent-nav-child').slideUp();
+
         $(this).find('.fa-angle-down').toggleClass('rotate-icon');
     });
+
+// Add Current Page in Sidebar
+    var CURRENT_URL=window.location.pathname;
+    var pathName = CURRENT_URL.slice(1);
+
+    var $Sidebar = $(".left-sidebar");
+    $Sidebar.find('a[href="'+pathName+'"]').parents(".parent-nav-child").show();
+    console.log(pathName);
+
+
 });
